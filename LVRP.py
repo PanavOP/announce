7e75ca8468a946dd70e87c6c13c987ecf2d72ed0
PanavOP <79737449+PanavOP@users.noreply.github.com>
2021-03-08 21:47:44 +0530

    Update LVRP.py

--- LVRP.py
+++ LVRP.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import time
+from discord import Member    
 from discord.ext.commands import has_permissions
+from discord.ext.commands import MissingPermissions
 
 client = commands.Bot(command_prefix = "+")
 client.remove_command("help")
@@ -17,10 +19,13 @@ async def on_ready():
     print("-------")
 
 @client.command(pass_context=True)
+@has_permissions(manage_roles=True, kick_members=True)
 async def lvrptext(ctx,*,message):
     await ctx.send(message)
 
+
 @client.command(pass_context=True)
+@has_permissions(manage_roles=True, kick_members=True)
 async def lvrpembed(ctx,*,message,):
     embed = discord.Embed(description=message,colour=0xFF0909)
     await ctx.send(embed=embed)
@@ -28,4 +33,6 @@ async def lvrpembed(ctx,*,message,):
 
 
 
+
 client.run("ODE1MTAyMjQ3MTMwMTAzODI4.YDnhHw.iFaucptuvPG1l5M7Idgqc_O4D14")
+
